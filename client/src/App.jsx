92b1e8987d3e507474b900a80a92f341801dcf64
IT-sun-code <orochimaro969@yandex.ru>
2024-02-15 23:18:25 +0300

    added info

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [info, setInfo] = useState(null);
+
+  const getInfo = () => {
+    fetch("http://localhost:3000/api/uni")
+      .then((res) => res.json())
+      .then((data) => setInfo(data))
+      .catch((err) => console.log(err));
+  };
 
   return (
     <>
+      <button onClick={getInfo}>получить данные</button>
+      <div>
+        {info && (
+          <>
+            <p>EngName: {info.EngName}</p>
+            <p>Fullname: {info.Fullname}</p>
+            <p>Name: {info.Name}</p>
+            <img src={info.Photo} alt="card" />
+          </>
+        )}
+      </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +48,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
