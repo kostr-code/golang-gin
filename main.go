2ece555a9ad2d264ae939889d462c25f2aa9022f
DerFreimensch <derfreimensch@yandex.ru>
2024-02-26 20:03:44 +0300

    Написал API для универов, Настя, проверяй)

--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "4Gi2u3p7"
+	dbname   = "UniSearch"
+)
+
+type UNI struct {
+	Name          string `json:"name"`
+	ID            int    `json:"id"`
+	Location      string `json:"location"`
+	Established   string `json:"established"`
+	Country       string `json:"country"`
+	City          string `json:"city"`
+	Main_language string `json:"main_language"`
+	Avg_price     string `json:"avg_price"`
+}
 
 func main() {
 	// Set the router as the default one shipped with Gin
@@ -32,12 +55,29 @@ func main() {
 func GetUni(c *gin.Context) {
 	c.Header("Content-Type", "application/json")
 	c.Header("Access-Control-Allow-Origin", "*")
-	c.JSON(http.StatusOK, gin.H{
-		"Name":     "Гуап",
-		"EngName":  "SUAI",
-		"Fullname": "Государственный Университет Аэрокосмического Приборостроения",
-		"Photo":    "/SUAI.jpeg",
-	})
+	//data := OpenDB()
+
+	var uni = []UNI{
+		{Name: "ГУАП", ID: 1, Location: "not found", Established: "1941", Country: "Россия", City: "Санкт-Петербург", Main_language: "Русский", Avg_price: "100000"},
+		{Name: "ИТМО", ID: 2, Location: "not found", Established: "1985", Country: "Россия", City: "Санкт-Петербург", Main_language: "Русский", Avg_price: "200000"},
+		{Name: "ВШЭ", ID: 3, Location: "not found", Established: "1995", Country: "Россия", City: "Санкт-Петербург", Main_language: "Русский", Avg_price: "250000"},
+		{Name: "ГУТ", ID: 4, Location: "not found", Established: "1937", Country: "Россия", City: "Санкт-Петербург", Main_language: "Русский", Avg_price: "135000"},
+		{Name: "ГАУ", ID: 5, Location: "not found", Established: "1869", Country: "Россия", City: "Санкт-Петербург", Main_language: "Русский", Avg_price: "88000"},
+		{Name: "ГЭУ", ID: 6, Location: "not found", Established: "1922", Country: "Россия", City: "Санкт-Петербург", Main_language: "Русский", Avg_price: "190000"},
+	}
+
+	// for data.Next() {
+	// 	var row UNI
+	// 	//log.Println(data)
+	// 	if err := data.Scan(&row.Name, &row.ID, &row.Location, &row.Established, &row.Country, &row.City, &row.Main_language, &row.Avg_price); err != nil {
+	// 		log.Println("Есть несоответствия: ")
+	// 		log.Println(err)
+	// 	} else {
+	// 		uni = append(uni, row)
+	// 	}
+	// }
+	log.Println(uni)
+	c.JSON(http.StatusOK, uni)
 }
 
 // LikeJoke increments the likes of a particular joke Item
@@ -46,4 +86,22 @@ func LikeJoke(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "LikeJoke handler not implemented yet",
 	})
+}
+
+func OpenDB() *sql.Rows {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	rows, err := db.Query("SELECT * FROM \"Uni\"")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	//log.Println(rows.Next())
+	return rows
 }
